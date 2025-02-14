d3ab154f499ade4c8f41131bb4933979130306dc
Nilson Pessim <nilson@techlabs.net.br>
2025-02-14 12:03:05 -0300

    update

--- custom_components/easymonitor/sensor.py
+++ custom_components/easymonitor/sensor.py
@@ -3,50 +3,68 @@ import voluptuous as vol
 
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.components.mqtt import async_subscribe
+from homeassistant.const import TEMP_CELSIUS, PERCENTAGE, ELECTRIC_POTENTIAL_VOLT
 
 _LOGGER = logging.getLogger(__name__)
 
+# Mapeamento dos sensores esperados
 SENSORS = {
-    "status": "Status",
-    "tempCH1": "Temperatura CH1",
-    "tempCH2": "Temperatura CH2",
-    "humiCH1": "Umidade CH1",
-    "humiCH2": "Umidade CH2",
-    "voltageDC0": "Tensão DC0",
-    "voltageDC1": "Tensão DC1",
-    "voltageAC0": "Tensão AC0"
+    "status": {"name": "Status", "unit": None, "icon": "mdi:information-outline"},
+    "tempCH1": {"name": "Temperatura CH1", "unit": TEMP_CELSIUS, "icon": "mdi:thermometer"},
+    "tempCH2": {"name": "Temperatura CH2", "unit": TEMP_CELSIUS, "icon": "mdi:thermometer"},
+    "humiCH1": {"name": "Umidade CH1", "unit": PERCENTAGE, "icon": "mdi:water-percent"},
+    "humiCH2": {"name": "Umidade CH2", "unit": PERCENTAGE, "icon": "mdi:water-percent"},
+    "voltageDC0": {"name": "Tensão DC0", "unit": ELECTRIC_POTENTIAL_VOLT, "icon": "mdi:flash"},
+    "voltageDC1": {"name": "Tensão DC1", "unit": ELECTRIC_POTENTIAL_VOLT, "icon": "mdi:flash"},
+    "voltageAC0": {"name": "Tensão AC0", "unit": ELECTRIC_POTENTIAL_VOLT, "icon": "mdi:flash"}
 }
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Configura a integração EasyMonitor."""
+    """Configura automaticamente os sensores MQTT do EasyMonitor."""
     mqtt = hass.components.mqtt
     entities = []
 
-    for sensor_id, name in SENSORS.items():
-        entities.append(EasyMonitorSensor(mqtt, name, sensor_id))
+    # Captura todos os dispositivos que publicam no tópico "easymonitor/{deviceID}/#"
+    async def device_discovered(msg):
+        topic_parts = msg.topic.split("/")
+        if len(topic_parts) != 3:
+            return
+        
+        device_id = topic_parts[1]
+        sensor_id = topic_parts[2]
+
+        if sensor_id in SENSORS:
+            entities.append(EasyMonitorSensor(mqtt, device_id, sensor_id))
+
+    await async_subscribe(hass, "easymonitor/+/status", device_discovered, 1)
 
     async_add_entities(entities)
 
 class EasyMonitorSensor(SensorEntity):
-    """Representa um sensor EasyMonitor que recebe dados via MQTT."""
+    """Representa um sensor EasyMonitor baseado em MQTT."""
 
-    def __init__(self, mqtt, name, sensor_id):
+    def __init__(self, mqtt, device_id, sensor_id):
+        """Inicializa o sensor."""
         self._mqtt = mqtt
-        self._name = name
-        self._state = None
+        self._device_id = device_id
         self._sensor_id = sensor_id
-        self._attr_name = f"EasyMonitor {name}"
-        self._attr_unique_id = f"easymonitor_{sensor_id}"
-        self._attr_device_class = "temperature" if "temp" in sensor_id else "voltage"
+        self._state = None
+        self._attr_name = f"{SENSORS[sensor_id]['name']} ({device_id})"
+        self._attr_unique_id = f"easymonitor_{device_id}_{sensor_id}"
+        self._attr_device_class = None
+        self._attr_native_unit_of_measurement = SENSORS[sensor_id]["unit"]
+        self._attr_icon = SENSORS[sensor_id]["icon"]
+
+    async def async_added_to_hass(self):
+        """Subscreve ao tópico MQTT correspondente."""
+        topic = f"easymonitor/{self._device_id}/{self._sensor_id}"
+
+        async def message_received(msg):
+            self._state = msg.payload
+            self.async_write_ha_state()
+
+        await async_subscribe(self.hass, topic, message_received, 1)
 
     @property
-    def icon(self):
-        """Define o ícone baseado no tipo do sensor."""
-        if "temp" in self._sensor_id:
-            return "mdi:thermometer"
-        elif "humi" in self._sensor_id:
-            return "mdi:water-percent"
-        elif "voltage" in self._sensor_id:
-            return "mdi:flash"
-        return "mdi:chip"
-
+    def state(self):
+        return self._state

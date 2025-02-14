377e09c0c4a9e9d001559da92dae23c80a1a1b2d
Nilson Pessim <nilson@techlabs.net.br>
2025-02-14 14:07:21 -0300

    update

--- custom_components/easymonitor/sensor.py
+++ custom_components/easymonitor/sensor.py
@@ -1,13 +1,11 @@
 import logging
 import voluptuous as vol
-
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.components.mqtt import async_subscribe
 from homeassistant.const import TEMP_CELSIUS, PERCENTAGE, ELECTRIC_POTENTIAL_VOLT
 
 _LOGGER = logging.getLogger(__name__)
 
-# Mapeamento dos sensores esperados
 SENSORS = {
     "status": {"name": "Status", "unit": None, "icon": "mdi:information-outline"},
     "tempCH1": {"name": "Temperatura CH1", "unit": TEMP_CELSIUS, "icon": "mdi:thermometer"},
@@ -16,29 +14,32 @@ SENSORS = {
     "humiCH2": {"name": "Umidade CH2", "unit": PERCENTAGE, "icon": "mdi:water-percent"},
     "voltageDC0": {"name": "Tensão DC0", "unit": ELECTRIC_POTENTIAL_VOLT, "icon": "mdi:flash"},
     "voltageDC1": {"name": "Tensão DC1", "unit": ELECTRIC_POTENTIAL_VOLT, "icon": "mdi:flash"},
-    "voltageAC0": {"name": "Tensão AC0", "unit": ELECTRIC_POTENTIAL_VOLT, "icon": "mdi:flash"}
+    "voltageAC0": {"name": "Tensão AC0", "unit": None, "icon": "mdi:flash"}
 }
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Configura automaticamente os sensores MQTT do EasyMonitor."""
     mqtt = hass.components.mqtt
-    entities = []
+    entities = {}
 
-    # Captura todos os dispositivos que publicam no tópico "easymonitor/{deviceID}/#"
+    # Descoberta de dispositivos no tópico "easymonitor/+/status"
     async def device_discovered(msg):
         topic_parts = msg.topic.split("/")
-        if len(topic_parts) != 3:
+        if len(topic_parts) < 3:
             return
-        
-        device_id = topic_parts[1]
-        sensor_id = topic_parts[2]
 
-        if sensor_id in SENSORS:
-            entities.append(EasyMonitorSensor(mqtt, device_id, sensor_id))
+        device_id = topic_parts[1]
+        
+        if device_id not in entities:
+            entities[device_id] = []
+            _LOGGER.info(f"Novo dispositivo detectado: {device_id}")
+
+            for sensor_id in SENSORS:
+                entity = EasyMonitorSensor(mqtt, device_id, sensor_id)
+                entities[device_id].append(entity)
+                async_add_entities([entity], True)
 
     await async_subscribe(hass, "easymonitor/+/status", device_discovered, 1)
-
-    async_add_entities(entities)
 
 class EasyMonitorSensor(SensorEntity):
     """Representa um sensor EasyMonitor baseado em MQTT."""
